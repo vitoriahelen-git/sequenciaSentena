01680294ec663269915e3d63c3753748a99a2c0c
Vitoria <vitoriahellen3004@gmail.com>
2020-11-20 16:26:57 -0300

    Sequencia 1 à 100

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sequenciaSentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1; 
+            while (numero <= 100)
+            {
+                Console.Write(numero);
+                Console.Write(" ");
+                numero += 1; 
+            }
+                Console.WriteLine("\nPara encerrar, pressione qualquer tecla"); 
+                Console.ReadLine(); 
+        }
+ 
+    }
+}
